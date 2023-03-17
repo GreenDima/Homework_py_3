a5af52f5891b8809d94856490efd1aa9ae01fbe4
Dmitry <greendima13@mail.ru>
2023-03-17 15:22:45 +0300

    Task 2

--- Homework_3.py
+++ Homework_3.py
@@ -12,3 +12,29 @@
 
 
 
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+import random
+list_length = int(input("Введите длину списка: "))
+list = []
+for i in range (list_length):
+    list.append(random.randint(1, 10))
+print(list)
+user_number = int(input("Введите число: "))
+min = abs(user_number - list[0])
+index = 0
+for i in range(list_length):
+    if user_number > list[i]:
+        if (user_number - list[i]) < min and (user_number - list[i]) != 0:
+            index = i
+            min = user_number - list[i]
+    if user_number < list[i]:
+        if (list[i] - user_number) < min and (list[i] - user_number) != 0:
+            index = i
+            min = user_number - list[i]
+print(f"Близкий элемент к вашему числу с индексом {index}")
+
+
+
